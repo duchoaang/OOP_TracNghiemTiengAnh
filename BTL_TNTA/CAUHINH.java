8c363ccba3645edfa89f560461f762ce28ce91bf
duchoaang <hoangbrato@gmail.com>
2023-12-24 17:59:31 +0700

    update file path

--- BTL_TNTA/CAUHINH.java
+++ BTL_TNTA/CAUHINH.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class CAUHINH {
     public static final Scanner sc = new Scanner(System.in);
     public static final SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
-    public static String filePath = "resources/multiple_choice.txt";
-    public static String filePath2 = "resources/conversation.txt";
-    public static String filePath3 = "resources/incomplete.txt";
+    public static String filePath = "BTL_TNTA/resources/multiple_choice.txt";
+    public static String filePath2 = "BTL_TNTA/resources/conversation.txt";
+    public static String filePath3 = "BTL_TNTA/resources/incomplete.txt";
     public static final double SO_DIEM = 10;
     public static final DecimalFormat df = new DecimalFormat("#.##");
 }
